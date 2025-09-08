5dd5ee6f8e52cb6625c3d754c68aea5077cfabd4
AleksanderKupriyanov <kupriyanov.a.a@yandex.ru>
2025-09-08 04:20:35 +0300

    Update Home.java

--- src/main/java/ru/aston/hometask/Home.java
+++ src/main/java/ru/aston/hometask/Home.java
@@ -130,7 +130,7 @@ public class Home {
             this.field15 = field15;
             return this;
         }
-
+        
         public Home build() {
             return new Home(field1, field2, field3, field4, field5, field6, field7, field8, field9, field10, field11, field12, field13, field14, field15);
         }
